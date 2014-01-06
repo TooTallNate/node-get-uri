83383e4cf478f7c5f79a8aed7d52afed9755ac3a
Bent Cardan <bent@kickr.io>
2014-01-06 14:40:59 -0500

    file protocol readable

--- file.js
+++ file.js
@@ -3,6 +3,8 @@
  * Module dependencies.
  */
 
+var fs = require('fs')
+
 /**
  * Module exports.
  */
@@ -14,4 +16,24 @@ module.exports = get;
  */
 
 function get (parsed, opts) {
+
+	//verify path with thrown err
+	fs.stat(parsed.path,function(err){
+		console.log('that file path was wrong',err)
+		if(err) throw err;
+	})
+
+	if (typeof arguments[1] != 'object'){
+		throw new TypeError('pass an object instead of: '+opts);
+	} else {
+
+		if(Object.keys(opts).length < 1){
+			//thanks for nothing
+			return fs.createReadStream(arguments[0].path)
+		} else {
+			//and we're ready to pipe
+			return fs.createReadStream(arguments[0].path,opts)
+		}
+
+	}
 }
