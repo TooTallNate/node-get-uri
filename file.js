c6db06cbdee2838b3e0d34596b312ea43fc04337
Bent Cardan <bent@kickr.io>
2014-01-08 22:41:00 -0800

    file: beginnings of "file:" protocol impl
    Squashed commit of the following:
    
    commit 153b8a8a5373f259f83f0b2c433094885e79acbe
    Author: Bent Cardan <reqshark@users.noreply.github.com>
    Date:   Mon Jan 6 16:20:42 2014 -0500
    
        Delete example.js
    
    commit 68caecf243b536847262e0c2779144096c54d802
    Author: Bent Cardan <bent@shakelaw.com>
    Date:   Mon Jan 6 16:16:47 2014 -0500
    
        nevermind, just throw error on bad paths
    
    commit cec944550b272a0e2d72262f343c925b08eb56e7
    Author: Bent Cardan <bent@shakelaw.com>
    Date:   Mon Jan 6 15:00:45 2014 -0500
    
        clean up error message
    
        keepin it more @TooTallNate, it's better written like this
    
    commit 83383e4cf478f7c5f79a8aed7d52afed9755ac3a
    Author: Bent Cardan <bent@kickr.io>
    Date:   Mon Jan 6 14:40:59 2014 -0500
    
        file protocol readable
    
    Closes #2.

--- file.js
+++ file.js
@@ -1,7 +1,8 @@
-
 /**
  * Module dependencies.
  */
+
+var fs = require('fs')
 
 /**
  * Module exports.
@@ -14,4 +15,24 @@ module.exports = get;
  */
 
 function get (parsed, opts) {
+
+	//verify path with thrown err
+	fs.stat(parsed.path,function(err){
+		 if(err) {
+		 	console.log('incorrect file path')
+		 	throw err;
+		 }
+	})
+
+	if (typeof arguments[1] != 'object'){
+		throw new TypeError('pass an object instead of: '+opts);
+	} else {
+
+		if(Object.keys(opts).length < 1){
+			return fs.createReadStream(arguments[0].path)
+		} else {
+			return fs.createReadStream(arguments[0].path,opts)
+		}
+
+	}
 }
