52a99747d2a816c2105cdeb37365b592f40e564a
Nathan Rajlich <nathan@tootallnate.net>
2014-01-10 14:35:54 -0800

    data: add NotModifierError() handling logic
    Taking an SHA1 shasum of the URI string before processing it,
    to potentially avoid the creation of the Buffer from the "data:" URI string.

--- data.js
+++ data.js
@@ -3,8 +3,11 @@
  * Module dependencies.
  */
 
+var crypto = require('crypto');
 var Readable = require('stream').Readable;
 var dataUriToBuffer = require('data-uri-to-buffer');
+var NotModifiedError = require('./notmodified');
+var debug = require('debug')('get-uri:ftp');
 
 /**
  * Module exports.
@@ -20,10 +23,28 @@ module.exports = get;
 
 function get (parsed, opts, fn) {
   var uri = parsed.href;
-  var buf = dataUriToBuffer(uri, opts);
-  var rs = new Readable();
-  rs._read = read(buf);
-  fn(null, rs);
+  var cache = opts.cache;
+
+  // need to create a SHA1 hash of the URI string, for cacheability checks
+  // in future `getUri()` calls with the same data URI passed in.
+  var shasum = crypto.createHash('sha1');
+  shasum.update(uri);
+  var hash = shasum.digest('hex');
+  debug('generated SHA1 hash for "data:" URI: "%s"', hash);
+
+  // check if the cache is the same "data:" URI that was previously passed in.
+  if (cache && cache.hash == hash) {
+    debug('got matching cache SHA1 hash: "%s"', hash);
+    fn(new NotModifiedError());
+  } else {
+    debug('creating Readable stream from "data:" URI buffer');
+    var buf = dataUriToBuffer(uri, opts);
+    var rs = new Readable();
+    rs._read = read(buf);
+    buf = null;
+    rs.hash = hash;
+    fn(null, rs);
+  }
 }
 
 /**
