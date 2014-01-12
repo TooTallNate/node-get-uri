ac3064e9516c8cce9173fe358628043dd613f1dd
Nathan Rajlich <nathan@tootallnate.net>
2014-01-11 18:21:50 -0800

    test: add more "data:" protocol tests

--- test/test.js
+++ test/test.js
@@ -28,9 +28,12 @@ describe('get-uri', function () {
 
   describe('"data:" protocol', function () {
 
+    var cache;
+
     it('should work for URL-encoded data', function (done) {
       getUri('data:,Hello%2C%20World!', function (err, rs) {
         if (err) return done(err);
+        cache = rs;
         streamToArray(rs, function (err, array) {
           if (err) return done(err);
           var buf = Buffer.concat(array);
@@ -49,6 +52,14 @@ describe('get-uri', function () {
           assert.equal('Hello, World!', buf.toString());
           done();
         });
+      });
+    });
+
+    it('should return ENOTMODIFIED for the same URI with `cache`', function (done) {
+      getUri('data:,Hello%2C%20World!', { cache: cache }, function (err, rs) {
+        assert(err);
+        assert.equal('ENOTMODIFIED', err.code);
+        done();
       });
     });
 
@@ -92,6 +103,7 @@ describe('get-uri', function () {
         done();
       });
     });
+
   });
 
   describe('"ftp:" protocol', function () {
