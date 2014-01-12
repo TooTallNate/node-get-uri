0cdddb48e8c1e6424826bcad6a9b20b2c1b2c632
Nathan Rajlich <nathan@tootallnate.net>
2014-01-11 17:59:23 -0800

    test: use "ftpd" for the "ftp:" protocol test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@
  */
 
 var fs = require('fs');
+var ftpd = require('ftpd');
 var getUri = require('../');
 var assert = require('assert');
 var streamToArray = require('stream-to-array');
@@ -75,15 +76,60 @@ describe('get-uri', function () {
 
   describe('"ftp:" protocol', function () {
 
+    var port;
+    var server;
+
+    before(function (done) {
+      var options = {
+        pasvPortRangeStart: 4000,
+        pasvPortRangeEnd: 5000,
+        getInitialCwd: function (socket, fn) {
+          fn(null, '/');
+        },
+        getRoot: function (socket) {
+          return __dirname;
+        }
+      };
+
+      var host = '127.0.0.1';
+      server = new ftpd.FtpServer(host, options);
+
+      server.on('client:connected', function(conn){
+        var username;
+        conn.on('command:user', function(user, success, failure) {
+          username = user;
+          success();
+        });
+        conn.on('command:pass', function(pass, success, failure){
+          success(username);
+        });
+      });
+
+      server.listen(0, function () {
+        port = server.server.address().port;
+        done();
+      });
+    });
+
+    after(function (done) {
+      server.server.once('close', function () {
+        done();
+      });
+      server.server.close();
+    });
+
     it('should work for ftp endpoints', function (done) {
-      var uri = 'ftp://ftp.kernel.org/pub/site/README';
-      getUri(uri, function (err, rs) {
+      var uri = 'ftp://127.0.0.1:' + port + '/test.js';
+      fs.readFile(__filename, 'utf8', function (err, real) {
         if (err) return done(err);
-        streamToArray(rs, function (err, array) {
+        getUri(uri, function (err, rs) {
           if (err) return done(err);
-          var buf = Buffer.concat(array);
-          assert(buf.length > 0);
-          done();
+          streamToArray(rs, function (err, array) {
+            if (err) return done(err);
+            var str = Buffer.concat(array).toString('utf8');
+            assert.equal(str, real);
+            done();
+          });
         });
       });
     });
