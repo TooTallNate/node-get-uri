069b93b9025f0f8e400a40d9d658b11613963b61
Nathan Rajlich <nathan@tootallnate.net>
2014-01-27 00:36:02 -0800

    test: fix "file:" URI tests on Windows

--- test/test.js
+++ test/test.js
@@ -71,9 +71,22 @@ describe('get-uri', function () {
   describe('"file:" protocol', function () {
 
     var cache;
+    var sep = require('path').sep || '/';
+
+    // TODO: move this out into a more full-featured module some day…
+    // i.e. the inverse of https://github.com/TooTallNate/file-path-to-uri
+    function path2uri (p) {
+      if ('\\' == sep) {
+        // windows
+        return 'file:///' + p;
+      } else {
+        // unix
+        return 'file://' + p;
+      }
+    }
 
     it('should work for local files', function (done) {
-      var uri = 'file://' + __filename;
+      var uri = path2uri(__filename);
       fs.readFile(__filename, 'utf8', function (err, real) {
         if (err) return done(err);
         getUri(uri, function (err, rs) {
@@ -90,7 +103,7 @@ describe('get-uri', function () {
     });
 
     it('should return ENOTFOUND for bad filenames', function (done) {
-      var uri = 'file://' + __filename + 'does-not-exist';
+      var uri = path2uri(__filename + 'does-not-exist');
       getUri(uri, function (err, rs) {
         assert(err);
         assert.equal('ENOTFOUND', err.code);
@@ -99,7 +112,7 @@ describe('get-uri', function () {
     });
 
     it('should return ENOTMODIFIED for the same URI with `cache`', function (done) {
-      var uri = 'file://' + __filename;
+      var uri = path2uri(__filename);
       getUri(uri, { cache: cache }, function (err, rs) {
         assert(err);
         assert.equal('ENOTMODIFIED', err.code);
