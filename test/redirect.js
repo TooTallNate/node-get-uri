a3594489d07438060ec1cf33342475cf368c4de0
Nathan Rajlich <nathan@tootallnate.net>
2019-10-10 12:17:36 -0700

    Use GH Actions

--- test/redirect.js
+++ test/redirect.js
@@ -16,77 +16,83 @@ describe('get-uri', function () {
 
   describe('http/https redirects', function () {
 
-    var options = {
-      key: fs.readFileSync(__dirname + '/server.key'),
-      cert: fs.readFileSync(__dirname + '/server.crt')
-    };
-
     var httpServer;
     var httpsServer;
     var httpPort;
     var httpsPort;
 
+    before(function(done) {
+      httpServer = http.createServer();
+      httpServer.listen(function() {
+        httpPort = httpServer.address().port;
+        done();
+      });
+    });
+
+    before(function(done) {
+      var options = {
+        key: fs.readFileSync(__dirname + '/server.key'),
+        cert: fs.readFileSync(__dirname + '/server.crt')
+      };
+      httpsServer = https.createServer(options);
+      httpsServer.listen(function() {
+        httpsPort = httpsServer.address().port;
+        done();
+      });
+    });
+
     after(function (done) {
       httpsServer.once('close', function () {
         done();
       });
+      httpsServer.close();
+    });
+
+    after(function (done) {
       httpServer.once('close', function () {
-        httpsServer.close();
+        done();
       });
       httpServer.close();
     });
 
     it('should handle http -> https redirect', function (done) {
-      httpsServer = https.createServer(options, st(__dirname));
-      httpsServer.listen(function () {
-        httpsPort = httpsServer.address().port;
-        httpServer = http.createServer(function(req, res) {
-          res.writeHead(301, {'Location': 'https://127.0.0.1:' + httpsPort + '/' + path.basename(__filename)});
-          res.end('Moved');
-        });
-        httpServer.listen(function () {
-          httpPort = httpServer.address().port;
-
-          var uri = 'http://127.0.0.1:' + httpPort + '/' + path.basename(__filename);
-          fs.readFile(__filename, 'utf8', function (err, real) {
+      httpsServer.once('request', st(__dirname));
+      httpServer.once('request', function(req, res) {
+        res.writeHead(301, {'Location': 'https://127.0.0.1:' + httpsPort + '/' + path.basename(__filename)});
+        res.end('Moved');
+      });
+      var uri = 'http://127.0.0.1:' + httpPort + '/' + path.basename(__filename);
+      fs.readFile(__filename, 'utf8', function (err, real) {
+        if (err) return done(err);
+        getUri(uri, { rejectUnauthorized: false }, function (err, rs) {
+          if (err) return done(err);
+          streamToArray(rs, function (err, array) {
             if (err) return done(err);
-            getUri(uri, { rejectUnauthorized: false }, function (err, rs) {
-              if (err) return done(err);
-              streamToArray(rs, function (err, array) {
-                if (err) return done(err);
-                var str = Buffer.concat(array).toString('utf8');
-                assert.equal(str, real);
-                done();
-              });
-            });
+            var str = Buffer.concat(array).toString('utf8');
+            assert.equal(str, real);
+            done();
           });
         });
       });
     });
 
     it('should handle https -> http redirect', function (done) {
-      httpServer = http.createServer(st(__dirname));
-      httpServer.listen(function () {
-        httpPort = httpServer.address().port;
-        httpsServer = https.createServer(options, function(req, res) {
-          res.writeHead(301, {'Location': 'http://127.0.0.1:' + httpPort + '/' + path.basename(__filename)});
-          res.end('Moved');
-        });
-        httpsServer.listen(function () {
-          httpsPort = httpsServer.address().port;
+      httpServer.once('request', st(__dirname));
+      httpsServer.once('request', function(req, res) {
+        res.writeHead(301, {'Location': 'http://127.0.0.1:' + httpPort + '/' + path.basename(__filename)});
+        res.end('Moved');
+      });
 
-          var uri = 'http://127.0.0.1:' + httpPort + '/' + path.basename(__filename);
-          fs.readFile(__filename, 'utf8', function (err, real) {
+      var uri = 'http://127.0.0.1:' + httpPort + '/' + path.basename(__filename);
+      fs.readFile(__filename, 'utf8', function (err, real) {
+        if (err) return done(err);
+        getUri(uri, function (err, rs) {
+          if (err) return done(err);
+          streamToArray(rs, function (err, array) {
             if (err) return done(err);
-            getUri(uri, function (err, rs) {
-              if (err) return done(err);
-              streamToArray(rs, function (err, array) {
-                if (err) return done(err);
-                var str = Buffer.concat(array).toString('utf8');
-                assert.equal(str, real);
-                done();
-              });
-            });
+            var str = Buffer.concat(array).toString('utf8');
+            assert.equal(str, real);
+            done();
           });
         });
       });
--- test/test.js
+++ test/test.js
@@ -3,14 +3,8 @@
  * Module dependencies.
  */
 
-var fs = require('fs');
-var st = require('st');
-var ftpd = require('ftpd');
-var http = require('http');
-var https = require('https');
 var getUri = require('../');
 var assert = require('assert');
-var streamToArray = require('stream-to-array');
 
 describe('get-uri', function () {
 
