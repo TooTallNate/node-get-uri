c207e2b7d20f08b4a0f6ff7ac7954c2528f59ea8
Nathan Rajlich <n@n8.io>
2023-04-25 17:45:44 -0700

    Switch to `basic-ftp`

--- src/ftp.ts
+++ src/ftp.ts
@@ -1,6 +1,5 @@
-import once from '@tootallnate/once';
-import FTP, { ListingElement, Options } from 'ftp';
-import { Readable } from 'stream';
+import { Client, AccessOptions } from 'basic-ftp';
+import { PassThrough, Readable } from 'stream';
 import { basename, dirname } from 'path';
 import createDebug from 'debug';
 import NotFoundError from './notfound';
@@ -13,9 +12,8 @@ export interface FTPReadable extends Readable {
 	lastModified?: Date;
 }
 
-export interface FTPOptions extends Options {
+export interface FTPOptions extends AccessOptions {
 	cache?: FTPReadable;
-	debug?: (s: string) => void;
 }
 
 /**
@@ -24,46 +22,32 @@ export interface FTPOptions extends Options {
 export const ftp: GetUriProtocol<FTPOptions> = async (url, opts = {}) => {
 	const { cache } = opts;
 	const filepath = url.pathname;
-	let lastModified: Date | null = null;
+	let lastModified: Date | undefined;
 
 	if (!filepath) {
 		throw new TypeError('No "pathname"!');
 	}
 
-	const client = new FTP();
-	client.once('greeting', (greeting: string) => {
-		debug('FTP greeting: %o', greeting);
-	});
-
-	function onend() {
-		// close the FTP client socket connection
-		client.end();
-	}
+	const client = new Client();
 
 	try {
-		opts.host = url.hostname || url.host || 'localhost';
-		opts.port = parseInt(url.port || '0', 10) || 21;
-		opts.debug = debug;
+		const host = url.hostname || url.host || 'localhost';
+		const port = parseInt(url.port || '0', 10) || 21;
+		const user = url.username || undefined;
+		const password = url.password || undefined;
 
-		if (url.username) {
-			opts.user = url.username;
-		}
-		if (url.password) {
-			opts.password = url.password;
-		}
-
-		const readyPromise = once(client, 'ready');
-		client.connect(opts);
-		await readyPromise;
+		await client.access({
+			host,
+			port,
+			user,
+			password,
+			...opts,
+		});
 
 		// first we have to figure out the Last Modified date.
 		// try the MDTM command first, which is an optional extension command.
 		try {
-			lastModified = await new Promise((resolve, reject) => {
-				client.lastMod(filepath, (err, res) => {
-					return err ? reject(err) : resolve(res);
-				});
-			});
+			lastModified = await client.lastMod(filepath);
 		} catch (err: any) {
 			// handle the "file not found" error code
 			if (err.code === 550) {
@@ -74,19 +58,13 @@ export const ftp: GetUriProtocol<FTPOptions> = async (url, opts = {}) => {
 		if (!lastModified) {
 			// Try to get the last modified date via the LIST command (uses
 			// more bandwidth, but is more compatible with older FTP servers
-			const list = await new Promise<ListingElement[]>(
-				(resolve, reject) => {
-					client.list(dirname(filepath), (err, res) => {
-						return err ? reject(err) : resolve(res);
-					});
-				}
-			);
+			const list = await client.list(dirname(filepath));
 
 			// attempt to find the "entry" with a matching "name"
 			const name = basename(filepath);
 			const entry = list.find((e) => e.name === name);
 			if (entry) {
-				lastModified = entry.date;
+				lastModified = entry.modifiedAt;
 			}
 		}
 
@@ -98,27 +76,22 @@ export const ftp: GetUriProtocol<FTPOptions> = async (url, opts = {}) => {
 			throw new NotFoundError();
 		}
 
-		// XXX: a small timeout seemed necessary otherwise FTP servers
-		// were returning empty sockets for the file occasionally
-		// setTimeout(client.get.bind(client, filepath, onfile), 10);
-		const rs = (await new Promise<NodeJS.ReadableStream>(
-			(resolve, reject) => {
-				client.get(filepath, (err, res) => {
-					return err ? reject(err) : resolve(res);
-				});
-			}
-		)) as FTPReadable;
-		rs.once('end', onend);
+		const stream = new PassThrough();
+		const rs = stream as FTPReadable;
+		client.downloadTo(stream, filepath).then((result) => {
+			debug(result.message);
+			client.close();
+		});
 		rs.lastModified = lastModified;
 		return rs;
 	} catch (err) {
-		client.destroy();
+		client.close();
 		throw err;
 	}
 
 	// called when `lastModified` is set, and a "cache" stream was provided
 	function isNotModified(): boolean {
-		if (cache && cache.lastModified && lastModified) {
+		if (cache?.lastModified && lastModified) {
 			return +cache.lastModified === +lastModified;
 		}
 		return false;
