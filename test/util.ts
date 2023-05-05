96f2f7cbf69ae6de0d307bcbbc74c10970fdc6e4
Nathan Rajlich <n@n8.io>
2023-05-04 17:58:55 -0700

    Add notice about move to monorepo

--- test/util.ts
+++
@@ -1,9 +0,0 @@
-import { Readable } from 'stream';
-
-export async function toBuffer(stream: Readable): Promise<Buffer> {
-	const chunks: Buffer[] = [];
-	for await (const chunk of stream) {
-		chunks.push(chunk);
-	}
-	return Buffer.concat(chunks);
-}
